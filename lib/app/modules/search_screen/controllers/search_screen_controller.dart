4f5b977d6d5c53a4d8081527bb3901dfdc4532fc
Aditya Chaudhary <adityachaudhary1029@gmail.com>
2022-09-25 05:54:01 +0530

    commit

---
+++ lib/app/modules/search_screen/controllers/search_screen_controller.dart
@@ -0,0 +1,20 @@
+import 'package:get/get.dart';
+
+class SearchScreenController extends GetxController {
+  //TODO: Implement SearchScreenController
+
+  final count = 0.obs;
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {}
+  void increment() => count.value++;
+}
